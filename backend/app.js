34ada2e5ca8cd16464469e8fc833a76788af59ee
moksh59022 <vsmokshsharma688@gmail.com>
2025-05-06 22:45:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const port=process.env.PORT || 4255;
 const app = express()
 app.use(express.json())
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/userapi');
 app.use('/api', userRoutes);
 
 
