2e0c8e125c72d83af51e295adf3ea38540cb30d8
moksh59022 <vsmokshsharma688@gmail.com>
2025-05-06 22:44:13 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const port=process.env.PORT || 4255;
 const app = express()
+const userRoutes = require('./routes/userapi')
 app.use(express.json())
-
+app.use('/', userRoutes);
 const db = require('./db')
 
 app.get('/',(req,res)=>{
